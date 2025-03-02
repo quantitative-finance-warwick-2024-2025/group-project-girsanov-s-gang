93088be3d9b8f805ada489041ff81dbfec2541f6
Kejing Wang <wkj200109@163.com>
2025-03-02 17:40:47 +0000

    Update OptionPricerBarrier.hpp

--- src/OptionPricerBarrier.hpp
+++ src/OptionPricerBarrier.hpp
@@ -1,12 +1,41 @@
-#ifndef OPTION_PRICER_BARRIER_HPP
-#define OPTION_PRICER_BARRIER_HPP
+#ifndef OPTIONPRICERBARRIER_HPP
+#define OPTIONPRICERBARRIER_HPP
 
-#include "Option.hpp"
 #include "BarrierOption.hpp"
+#include <vector>
 
 class OptionPricerBarrier {
 public:
-    static double calculatePriceBarrierNaive(const Option &option, double stock_price, double rf_rate, double volatility, unsigned int num_of_simulations);
+    // Pricing methods
+    static double calculatePriceNaive(const BarrierOption& option, double S0,
+                                     double r, double sigma, unsigned int numSimulations);
+    
+    static double calculatePriceAntithetic(const BarrierOption& option, double S0,
+                                          double r, double sigma, unsigned int numSimulations);
+    
+    static double calculatePriceImportanceSampling(const BarrierOption& option, double S0,
+                                                  double r, double sigma, unsigned int numSimulations);
+    
+    static double calculatePriceControlVariates(const BarrierOption& option, double S0,
+                                               double r, double sigma, unsigned int numSimulations);
+
+private:
+    // Path generation structure
+    struct PathResult {
+        double finalPrice;
+        double pathExtreme;
+        double weight;
+    };
+
+    // Core path generation method
+    static void generatePath(const BarrierOption& option, double S0, double r, double sigma,
+                            PathResult& result, double driftAdjust = 0.0);
+    
+    // Helper methods
+    static double blackScholesPrice(double S, double K, double T, 
+                                   double r, double sigma, Option::Type type);
+    static double normalCDF(double x);
+    static double inverseNormalCDF(double u);
 };
 
-#endif // OPTION_PRICER_BARRIER_HPP
+#endif
